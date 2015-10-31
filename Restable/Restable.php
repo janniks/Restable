a5d53d8e11a907ba097c5943484d07a17ce4d719
janniks <jannik.sieb@gmail.com>
2015-10-31 17:34:51 +0100

    update path regex

--- Restable/Restable.php
+++ Restable/Restable.php
@@ -119,7 +119,7 @@ class Restable {
 
 		// match resource to path
 		$matches = array();
-		preg_match("!^(?'path'[^\:]*\/)(?'var'\:[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)?$!", $resource, $matches);
+		preg_match("!^(?'path'[^\:]*\/?)(?'var'\:[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)?$!", $resource, $matches);
 
 		// abort if path not valid
 		if (empty($matches) || !isset($matches['path']))
