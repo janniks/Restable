b480a069f7d63bec7e99cecdcf6d6f252f7e5305
janniks <jannik.sieb@gmail.com>
2016-01-23 00:32:16 +0100

    simplify example index.php

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php
 
 require_once 'Restable/Restable.php';
+$app = new Restable();
 
-function what_a_function() {
-	echo 'such fun!';
-}
+$app->get('/', function() {
+    echo 'It works!';
+});
 
-$api = new Restable();
-
-$api->bad_path = function() {
-	echo 'new bad path';
-};
-
-$api->get('/test/:id', function($id) use ($api) {
-	$api->json(array(
-		'id' => $id,
-	));
-}, array(
-	'before' => 'what_a_function',
-));
-
-$api->start();
+$app->start();
